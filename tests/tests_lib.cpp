10e1c1ff09c9bb85fffd028a543a8bf20db6cfe4
magenta-tomato <91065723+magenta-tomato@users.noreply.github.com>
2024-02-18 16:42:03 +0300

    Update tests_lib.cpp

--- tests/tests_lib.cpp
+++ tests/tests_lib.cpp
@@ -12,8 +12,39 @@ BOOST_AUTO_TEST_CASE(test_valid_version)
 
 BOOST_AUTO_TEST_CASE(test1)
 {
-	bool res = true;
-	BOOST_CHECK(res);
+	unit_test_log.set_threshold_level(log_messages);
+	BOOST_TEST_MESSAGE("test1");
+	
+	// бесконечная матрица int заполнена значениями - 1
+	Matrix<int, -1> matrix;
+
+	matrix[0][0] = -1;
+	assert(matrix.size() == 0); // все ячейки свободны
+	auto a = matrix[0][0];
+	BOOST_CHECK(a == -1);
+	BOOST_CHECK(matrix.size() == 0);
+	matrix[100][100] = 314;
+	BOOST_CHECK(matrix[100][100] == 314);
+	BOOST_CHECK(matrix.size() == 1);
+	// выведется одна строка
+	// 100100314
+	string str;
+	for (auto i : matrix) {
+		int x;
+		int y;
+		int v;
+		//tie(x, y, v) = i;
+		//cout << i;
+
+		x = i.first.first;
+		y = i.first.second;
+		v = i.second;
+		//cout << x << y << v << endl;
+		str += x;
+		str += y;
+		str += v;
+	}
+	BOOST_CHECK( str.compare("100100314") == 0 );
 }
 
 
